ec82ca6796c5c9206e50e9270adc34bb15b80786
davidbridges123 <david.bridges@live.com>
2019-08-05 15:46:31 -0600

    adding variables and connection to db

---
+++ bamazonCustomer.js
@@ -0,0 +1,27 @@
+var inquirer = require("inquirer");
+
+var mysql = require("mysql");
+
+// global variables for storing item_id and stock_quantity for later checks
+var clientItem;
+var clientQuantity;
+var dbQuantity;
+var totalCost;
+
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "yourRootpassword",
+    database: "bamazon_DB"
+
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // console.log("connected as id " + connection.threadId);
+})
+
+
