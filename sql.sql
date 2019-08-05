ff7a8cdd467a43ed142e0593e55ec30c55912bf3
davidbridges123 <david.bridges@live.com>
2019-08-05 15:21:54 -0600

    making table

--- sql.sql
+++ sql.sql
@@ -1,4 +1,19 @@
--- Create a new database called 'DatabaseName'
--- Connect to the 'master' database to run this snippet
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
 
-CREATE DATABASE DatabaseName
+USE bamazon;
+
+CREATE TABLE top5000 (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM top5000;
