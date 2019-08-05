2677b788c70b79d2dbd0c32ddd06329460cc85fc
davidbridges123 <david.bridges@live.com>
2019-08-05 16:24:44 -0600

    added query loop

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,7 +26,17 @@ connection.connect(function (err) {
 
 
 function queryItems {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("-------------------------------");
+        //loop through all the items in stock and display them
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " in stock");
+            console.log("---------------------------------")
+        }
+    
+    
+    }
 
 
-
-}
+};
