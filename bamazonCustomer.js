dcba7cdafc9525b629c9cb254f245d7619f8659b
davidbridges123 <david.bridges@live.com>
2019-08-05 16:18:37 -0600

    change

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,12 +21,12 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    // console.log("connected as id " + connection.threadId);
+    queryItems();
 })
 
 
 function queryItems {
 
 
-    
+
 }
